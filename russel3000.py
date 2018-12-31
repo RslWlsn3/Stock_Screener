ee1db9f2fc91e8f6db744f2873ce6ce61984750e
Connor Mote <connor27mote@gmail.com>
2018-12-31 10:17:42 -0700

    Added Russel3000 option

---
+++ russel3000.py
@@ -0,0 +1,21 @@
+
+tickers = []
+
+def parse_rus3000():
+    try:
+        readFile = open('Russel3000_tickers.txt').read()
+        splitFile = readFile.split('\n')
+        for eachline in splitFile:
+            splitLine = eachline.split(' ')
+            ticker = splitLine[-1]
+            if ticker:     #gets rid of white space
+                tickers.append(ticker)
+        print(tickers)                
+    except Exception as e:
+        print(e)
+
+parse_rus3000()
+
+
+
+
